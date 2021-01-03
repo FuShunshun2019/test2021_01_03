25c45c968e628b17132e3adcd5605b2ff77c6ad8
FuShunshun2019 <75267291+FuShunshun2019@users.noreply.github.com>
2021-01-03 20:18:20 +0800

    Update zupu.js

--- src/assets/store/zupu.js
+++ src/assets/store/zupu.js
@@ -51,7 +51,7 @@ const zupu = {
                 member:[
                   {
                     num: '006',
-                    name: '张三儿子',
+                    name: '张三儿子222',
                     sex: '男'
                   },
                   {
