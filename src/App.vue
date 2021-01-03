5a21b589f3d896ee1fc77cc7c9b53f7628baa9c1
fuxiaomi <180.@163.com>
2021-01-03 18:42:24 +0800

    修改背景色为蓝色了

--- src/App.vue
+++ src/App.vue
@@ -31,13 +31,13 @@
     }
 
     .haha{
-      background-color:red;
+      background-color:blue;
     }
 
 </style>
 <template>
   <div class="family_container">
-      <p class="haha">哈哈哈哈哈哈</p>
+      <p class="haha">嘻嘻嘻</p>
       <h3> {{ title }} </h3>
       <div class="zupuBox">
         <!-- 每一代的人员，以代为单位 -->
